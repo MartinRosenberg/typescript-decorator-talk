53807451bfe85ad0f87496d648951b97e943c1c6
Han Yan <hanprogramming@gmail.com>
2020-03-13 21:33:37 -0600

    added comment for decorator order of operations

--- index.ts
+++ index.ts
@@ -1,4 +1,7 @@
 class Greeter {
+    /**
+     * IMPORTANT: Multiple decorators run in "inside out" order
+     */
     @AddIAm('Han')
     @AddNameCustom('Josh') // "Decorating a method"
     greet() {
