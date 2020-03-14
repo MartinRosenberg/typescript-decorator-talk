56920e1ce409aef0a71682632e793af29170033d
Han Yan <hanprogramming@gmail.com>
2020-03-13 20:04:53 -0600

    Decorator Intro for Methods

--- index.ts
+++ index.ts
@@ -1,7 +1,16 @@
 class Greeter {
+    @addName // "Decorating a method"
     greet() {
         return 'Hello';
     }
+}
+  
+function addName(constructor, methodName, methodDesc) {
+    // This is the decorator itself 
+    const originalMethod = methodDesc.value;
+    const newMethodDesc = {...methodDesc}; // Cloning the method
+    newMethodDesc.value = () => `${originalMethod()} Bootcampers` // Replacing the old method with a new method
+    return newMethodDesc;
 }
 
 function buttonPress() {
